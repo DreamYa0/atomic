8967da45968eb1e84006f21dbcfadabfdb37de0c
DreamYao <805229888@qq.com>
2018-06-01 18:30:17 +0800

    代码修改

--- src/main/resources/template/BaseTest.java
+++
@@ -1,7 +0,0 @@
-package com.atomic.autotest;
-
-import com.atomic.BaseDubbo;
-
-public abstract class BaseNgTest<T> extends BaseDubbo<T> {
-
-}
--- src/main/resources/template/Methods.java
+++
@@ -1,6 +0,0 @@
-//Ng
-	@Test(dataProvider = "excel")
-	public void testCase(Map<String, Object> context, Object result){
-
-	}
-//Ng End
