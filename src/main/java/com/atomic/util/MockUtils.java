8381f2d6642956d99ec406a8e6d1669743cd1a1d
DreamYao <805229888@qq.com>
2018-06-10 12:03:21 +0800

    代码修改

--- src/main/java/com/atomic/util/MockUtils.java
+++ src/main/java/com/atomic/util/MockUtils.java
@@ -20,7 +20,7 @@ public final class MockUtils {
     private static final Logger logger = LoggerFactory.getLogger(MockUtils.class);
 
     /**
-     * 方法mock
+     * 普通对象的方法mock
      * <pre>
      *     // mock SpringBean、mock工具类、mock普通类
      *     @Transactional
@@ -137,6 +137,54 @@ public final class MockUtils {
             new Expectations() {
                 {
                     method.invoke(mockInstance, mockParamter);
+
+                    // 有返回值的方法
+                    if (Boolean.FALSE.equals("void".equals(method.getReturnType().getName()))) {
+                        result = mockReturn;
+                    }
+                }
+            };
+        } catch (Exception e) {
+            logger.error("mock方法失败，mock方法名称为：{}", mockClass.getSimpleName() + "." + mockMethod, e);
+        }
+    }
+
+    /**
+     * class对象mock
+     * <pre>
+     *     // mock工具类、mock无法实列化的类
+     *     @Transactional
+     *     @SpringBootTest(classes = WebApplication.class)
+     *     public class TestUserSymbolAdd extends BaseTestCase<MemberUserController> {
+     *
+     *         @Override
+     *         public void beforeTest(Map<String, Object> context) {
+     *
+     *             // mock工具类
+     *             MockUtils.mock(MemberCheckUtil.class, "getUserNo", "mock返回值", ""mock入参");
+     *         }
+     *
+     *         @Test(dataProvider = Data.SINGLE,enabled = true)
+     *         public void testCase(Map<String, Object> context, Object result) {
+     *
+     *         }
+     *     }
+     * </pre>
+     * @param mockClass mock class对象
+     * @param mockMethod   mock方法名称
+     * @param mockReturn   mock返回结果
+     * @param mockParamter mock入参
+     */
+    public static void mockClass(Class<?> mockClass, String mockMethod, Object mockReturn, Object... mockParamter) {
+        Assert.notNull(mockClass, "Mock类不能为空");
+        Assert.notNull(mockMethod, "Mock方法名称不能为空");
+
+        try {
+            Method method = ReflectionUtils.getMethod(mockClass, mockMethod);
+            // 静态方法mock
+            new Expectations(mockClass) {
+                {
+                    MethodUtils.invokeStaticMethod(mockClass, mockMethod, mockParamter);
 
                     // 有返回值的方法
                     if (Boolean.FALSE.equals("void".equals(method.getReturnType().getName()))) {
