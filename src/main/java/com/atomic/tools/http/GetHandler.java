ee8980ba7f3764eaaf3c6ab3b07460335b432d84
yangminhan <yangminhan@g7.com.cn>
2021-01-29 19:41:45 +0800

    rest请求移除关键字expectResult

--- src/main/java/com/atomic/tools/http/GetHandler.java
+++ src/main/java/com/atomic/tools/http/GetHandler.java
@@ -33,6 +33,9 @@ public class GetHandler implements IHandler {
                 Map<String, Object> newParam = ParamUtils.getParameters(newContext);
                 // TODO: 2018/6/9 递归处理map
                 if (!newParam.isEmpty()) {
+                    if (newParam.containsKey("expectResult")) {
+                        newParam.remove("expectResult");
+                    }
                     request.form(newParam);
                 }
                 return request.execute();
